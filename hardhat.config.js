fd45482c53ec35b1500862a31a3bcf9d42208cc8
Kehinde. A <47582188+LegatoReign@users.noreply.github.com>
2022-09-21 19:34:06 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,47 +1,44 @@
-require('@nomicfoundation/hardhat-toolbox')
-require('dotenv').config()
-require('hardhat-gas-reporter')
-require('solidity-coverage')
-require('@nomiclabs/hardhat-etherscan')
-require('hardhat-deploy')
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
 
-const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY // || 'key'
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
 
-const LOCALHOST_RPC_URL = process.env.LOCALHOST_RPC_URL // || 'https://eth-rinkeby/example'
-
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL // || 'https://eth-rinkeby/example'
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY // || '0xkey'
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY // || 'key'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY
 
 module.exports = {
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   networks: {
-    rinkeby: {
-      url: RINKEBY_RPC_URL,
-      accounts: [RINKEBY_PRIVATE_KEY], //ethers.getSigners()
-      chainId: 4,
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [GOERLI_PRIVATE_KEY],
+      chainId: 5,
       blockConfirmation: 6,
     },
-    localhost: {
-      url: LOCALHOST_RPC_URL,
-      // accounts: [LOCALHOST_PRIVATE_KEY], HardHAt provides account automatically
+    hardhat: {
+      blockConfirmation: 1,
       chainId: 31337,
     },
   },
   // solidity: '0.8.8',
   solidity: {
-    compilers: [{ version: '0.8.8' }, { version: '0.6.6' }],
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
   gasReporter: {
     enable: false,
-    outputFile: 'gas-report.txt',
+    outputFile: "gas-report.txt",
     noColors: true,
-    currency: 'USD',
+    currency: "USD",
     coinmarketcap: COINMARKET_API_KEY,
-    // toekn: 'MATIC',
+    // token: 'MATIC',
   },
   namedAccounts: {
     deployer: {
@@ -50,5 +47,8 @@ module.exports = {
     user: {
       default: 1,
     },
+  },
+  mocha: {
+    timeout: 300000,
   },
 }
